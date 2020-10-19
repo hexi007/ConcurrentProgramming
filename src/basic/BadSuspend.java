dfb577be77b7428740556a15694b2fee6bf586c2
hexi007 <2777125829@qq.com>
2020-10-19 19:25:34 +0800

    挂起和继续执行

---
+++ src/basic/BadSuspend.java
@@ -0,0 +1,40 @@
+package basic;
+
+/**
+ * description 挂起和继续执行
+ *
+ * @author 27771
+ * create 2020-10-19 19:06
+ **/
+public class BadSuspend {
+    private static Object object = new Object();
+    private static ChangeObjectThread t1 = new ChangeObjectThread("t1");
+    private static ChangeObjectThread t2 = new ChangeObjectThread("t2");
+
+    private static class ChangeObjectThread extends Thread{
+        public ChangeObjectThread(String name){
+            super.setName(name);
+        }
+
+        @Override
+        public void run() {
+            synchronized (object){
+                System.out.println("in " + getName());
+                //suspend不会释放任何锁资源，如果resume操作先于suspend前执行，那么被挂起线程很难有机会执行
+                //被挂起线程状态还是Runnable，这会影响对系统当前状态的判断
+                Thread.currentThread().suspend();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        t1.start();
+        Thread.sleep(100);
+        t2.start();
+        //因为resume调用顺序的问题，resume没有生效，并导致t2永远挂起并占用了t1的锁
+        t1.resume();
+        t2.resume();
+        t1.join();
+        t2.join();
+    }
+}
